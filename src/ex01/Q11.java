4e4b750662b74a33c7f3d63ae11e02157b1badca
kwonbumjoon <bumjoon2010@gmail.com>
2024-01-24 18:35:33 +0900

    first commit

---
+++ src/ex01/Q11.java
@@ -0,0 +1,20 @@
+package ex01;
+
+public class Q11 {
+	public static void main(String[] args) {
+		
+		int[] coinUnit = {500, 100, 50, 10};
+		int money = 560;
+		System.out.println("money = " + money);
+		int leftmoney = 0;
+		for(int i=0; i<coinUnit.length; i++) {
+			if(i==0)
+				leftmoney = (money / coinUnit[i]);
+			else {
+				leftmoney = (money % coinUnit[i-1]) / coinUnit[i];
+			}
+			System.out.println(coinUnit[i] + "원 : " + leftmoney);
+		}
+			
+	}
+}
